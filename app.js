ab17ec7d0d69b5b315a040ca082de880aa992081
nimnadee-203 <126863860+nimnadee-203@users.noreply.github.com>
2026-02-23 15:58:43 +0530

    HTTP request example

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-const fs = require('fs');
+const https = require('https');
 
-fs.readFile('file.txt', 'utf8', function (err, data) {
-  if (err) throw err;
-  console.log(data);
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+  let data = '';
+
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on('error', (err) => {
+  console.log("Error: " + err.message);
 });
-
-fs.writeFile('file.txt', 'Hello World!', function (err) {
-  if (err) throw err;
-  console.log('File saved!');
-});
-
-const http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!');
-  res.end();
-}).listen(8081);
-
-console.log("Server running on port 8081");
