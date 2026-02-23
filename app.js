12de50f208e37881dc735bad52be55e8d1a5dbb0
nimnadee-203 <126863860+nimnadee-203@users.noreply.github.com>
2026-02-23 15:44:52 +0530

    update code

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-console.log("SE3040 Lab 03 - NodeJS Initialized");
-
-const fs = require('fs');
-
-fs.readFile('file.txt', 'utf8', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
 
 fs.writeFile('file.txt', 'Hello World!', function (err) {
   if (err) throw err;
