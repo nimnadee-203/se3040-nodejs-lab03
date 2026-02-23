07bf59b4cc8fc2c2a28db28a3e08240ed16e7a1e
nimnadee-203 <126863860+nimnadee-203@users.noreply.github.com>
2026-02-23 15:41:35 +0530

    HTTP request example

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ fs.writeFile('file.txt', 'Hello World!', function (err) {
   if (err) throw err;
   console.log('File saved!');
 });
+
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8081);
+
+console.log("Server running on port 8081");
