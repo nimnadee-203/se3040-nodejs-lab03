2c3229cc205b87240bd3dbc98b1cfd290de35541
nimnadee-203 <126863860+nimnadee-203@users.noreply.github.com>
2026-02-23 15:16:13 +0530

    Read file example

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("SE3040 Lab 03 - NodeJS Initialized");
+
+const fs = require('fs');
+
+fs.readFile('file.txt', 'utf8', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
