c2e69574e7a2b190f570835dd4f8c6df28e97500
nimnadee-203 <126863860+nimnadee-203@users.noreply.github.com>
2026-02-23 16:13:05 +0530

    example async await

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-const fs = require('fs');
+const condition = true;
 
-fs.readFile('file.txt', 'utf8', function (err, data) {
-  if (err) throw err;
-  console.log(data);
+const myPromise = new Promise((resolve, reject) => {
+  condition ? resolve("Success!") : reject("Failure!");
 });
 
-fs.writeFile('file.txt', 'Hello World!', function (err) {
-  if (err) throw err;
-  console.log('File saved!');
-});
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-const http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!');
-  res.end();
-}).listen(8081);
-
-console.log("Server running on port 8081");
-
-const myModule = require('./my-module');
-console.log(myModule.myFunction());
+myFunction();
